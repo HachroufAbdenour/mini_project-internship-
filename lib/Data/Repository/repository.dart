8c4e362a4ece89928931a2fc09d7d862a77432d7
HachroufAbdenour <abdouopt5@gmail.com>
2023-04-12 23:43:12 +0100

    save data into isar db succussfuly after fetching from web services  and handling #{{Repository}}#

--- lib/Data/Repository/repository.dart
+++ lib/Data/Repository/repository.dart
@@ -15,14 +15,20 @@ class Repository {
   Future<List<User>> getUsers() async {
     try {
       final response = await apiProvider.getUser();
-      return response;
 
 // call method to convert User Object To UserModel Object
 
-      // final userlocal =response.map((user) => convertUserToUserLocal(user)).toList();
+      final userlocal =
+          response.map((user) => convertUserToUserLocal(user)).toList();
+
+      print(" call method to convert User Object To UserModel Object");
 
 // save list of users into database isardb
-      // await saveUsers(userlocal);
+
+      await saveUsers(userlocal);
+//
+
+      return response;
     } catch (e) {
       throw Exception(e.toString());
     }
